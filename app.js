53ed15e7a58ff4ae6b27bd9d37486ad26895b378
Ron Martin <ronmartin89@gmail.com>
2020-02-18 14:19:58 -0800

    fully functional!

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const playstore = require('./playstore.data');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/apps', (req, res) => {
+  const {sort, genres} = req.query;
+  const validGenres = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
+  const validSorts = ['rating', 'app'];
+  let results = [...playstore];
+
+  if (sort) {
+    if(!validSorts.includes(sort.toLowerCase())) {
+      return res.status(400)
+        .send('Sort must be by rating or app.');
+    }
+  }
+  if (genres) {
+    if(!validGenres.includes(genres.toLowerCase())) {
+      return res.status(400)
+        .send(`Genres must be one of the following: ${validGenres}`);
+    }
+  }
+
+  if(req.query.genres) {
+    results = playstore
+      .filter(app => 
+        app
+          .Genres
+          .toLowerCase()
+          .includes(genres.toLowerCase()));
+  }
+
+  if (req.query.sort) {
+    let formattedInput = sort.charAt(0).toUpperCase() + sort.slice(1);
+    results.sort((a, b) => {
+      return a[formattedInput] < b[formattedInput] ? -1 : 1;
+    });
+  }
+
+  res.json(results);
+});
+
+app.listen(8000, () => {
+  console.log('Express is listening on port 8000');
+});
