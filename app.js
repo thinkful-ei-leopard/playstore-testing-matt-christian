96e0e48c90a048048400b6c8a3359349eca0a972
Ron Martin <ronmartin89@gmail.com>
2020-02-18 14:48:17 -0800

    added comments describing what each if statement is doing

--- app.js
+++ app.js
@@ -14,19 +14,18 @@ app.get('/apps', (req, res) => {
   const validSorts = ['rating', 'app'];
   let results = [...playstore];
 
-
+  // validating input: if sort exists, it must be 'rating' or 'app'
   if(sort && !validSorts.includes(sort.toLowerCase())) {
     return res.status(400)
       .json({ message: 'Sort must be by rating or app.'});
   }
-  
-
+  // validating input: if genres exists, it must be one of the values in the validGenres array
   if(genres && !validGenres.includes(genres.toLowerCase())) {
     return res.status(400)
       .json({ message: `Genres must be one of the following: ${validGenres}`});
   }
   
-
+  // if genres is valid, filter the list by the given genre
   if(req.query.genres) {
     results = playstore
       .filter(app => 
@@ -36,8 +35,11 @@ app.get('/apps', (req, res) => {
           .includes(genres.toLowerCase()));
   }
 
+  // if sort is valid, sort the list by either rating or app
   if (req.query.sort) {
-    let formattedInput = sort.charAt(0).toUpperCase() + sort.slice(1);
+    // since the properties on sort.App are all capitalized, we need to format the value of sort 
+    // (which might be Rating or rating or RaTiNg)
+    let formattedInput = sort.charAt(0).toUpperCase() + sort.slice(1).toLowerCase();
     results.sort((a, b) => {
       return a[formattedInput] < b[formattedInput] ? -1 : 1;
     });
